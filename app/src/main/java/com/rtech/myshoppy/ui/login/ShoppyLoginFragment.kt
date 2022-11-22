f6eaf238c94af5e53ae5c07056979ce767ccca9f
Vishal Rana <job.vishalrana@gmail.com>
2022-11-22 20:47:14 +0530

    navigation code updation

--- app/src/main/java/com/rtech/myshoppy/ui/login/ShoppyLoginFragment.kt
+++ app/src/main/java/com/rtech/myshoppy/ui/login/ShoppyLoginFragment.kt
@@ -6,20 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.rtech.myshoppy.R
+import com.rtech.myshoppy.databinding.FragmentShoppyLoginBinding
 
 class ShoppyLoginFragment : Fragment() {
+    private lateinit var binding: FragmentShoppyLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_shoppy_login, container, false)
+        savedInstanceState: Bundle?): View {
+        binding = FragmentShoppyLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<TextView>(R.id.txtHeading).text = arguments?.getString("title")
+
+        binding.txtSignUp.setOnClickListener{
+            findNavController().navigate(
+                R.id.shoppyRegisterFragment
+            )
+        }
     }
 }
