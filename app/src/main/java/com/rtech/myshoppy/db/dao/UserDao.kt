293a3a853620e2b180dbdfa70fc68a45c6c21723
Vishal Rana <job.vishalrana@gmail.com>
2022-11-23 20:40:29 +0530

    create db class and user table

---
+++ app/src/main/java/com/rtech/myshoppy/db/dao/UserDao.kt
@@ -0,0 +1,11 @@
+package com.rtech.myshoppy.db.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import com.rtech.myshoppy.db.entities.UserDetailsModel
+
+@Dao
+interface UserDao {
+    @Insert
+    fun addUser(user: UserDetailsModel): Int
+}
---
+++ app/src/main/java/com/rtech/myshoppy/db/entities/UserDetailsModel.kt
@@ -0,0 +1,14 @@
+package com.rtech.myshoppy.db.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_tbl")
+data class UserDetailsModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    var uname: String,
+    var dob: String,
+    var mobile: String,
+    var password: String
+)
